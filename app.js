d075f1759ba112a25cd00428a9b2629bc6faeea3
kevin-murithi <murithikevin54@gmail.com>
2024-09-30 23:58:19 +0300

    FINAL COMMIT

--- app.js
+++ app.js
@@ -77,4 +77,5 @@ app.listen(PORT, () => {
     console.log(`Server is running on http://localhost:${PORT}`);
     console.log('Your app is finally complete!!!!');
     console.log('System ready for deployment');
+    console.log('System ready for deployment');
 });
